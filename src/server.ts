1b159014b43dcc5ec5074a6c72b817b3336ece35
Habibtusher <tusherhrt@gmail.com>
2023-05-29 11:45:47 +0600

    setup express mongoose cors dotenv parse done

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config/index";
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    console.log("database connected");
+    app.listen(config.port, () => {
+      console.log(`Applicationlistening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log("faield to connect db");
+  }
+}
+main();
