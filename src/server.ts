80db5e8b46da15389796eb84ca5228b36df3a439
Habibtusher <tusherhrt@gmail.com>
2023-06-01 22:57:39 +0600

    lint:check

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,10 @@ import app from './app'
 import config from './config/index'
 async function main() {
   try {
-    await mongoose.connect(config.database_url as string)
+              await mongoose.connect(config.database_url as string)
     console.log('database connected')
     app.listen(config.port, () => {
-      console.log(`Applicationlistening on port ${config.port}`)
+      console.log(`Application listening on port ${config.port}`)
     })
   } catch (error) {
     console.log('faield to connect db')
