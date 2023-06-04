e382218a2d438d78a154d450600d8a124eb8ea33
Habibtusher <tusherhrt@gmail.com>
2023-06-04 20:24:26 +0600

    logger

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config/index'
-import logger from './shared/logger'
+import { logger, errrorLogger } from './shared/logger'
 async function main() {
   try {
     await mongoose.connect(config.database_url as string)
@@ -10,7 +10,7 @@ async function main() {
       logger.info(`Application listening on port ${config.port}`)
     })
   } catch (error) {
-    logger.error('faield to connect db', error)
+    errrorLogger.error('faield to connect db', error)
   }
 }
 main()
--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -1,13 +1,63 @@
-import winston from 'winston'
-const logger = winston.createLogger({
+import { createLogger, transports, format } from 'winston'
+import path from 'path'
+const { combine, timestamp, label, printf, prettyPrint } = format
+import DailyRotateFile from 'winston-daily-rotate-file'
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  const date = new Date(timestamp)
+  const hour = date.getHours()
+  const minutes = date.getMinutes()
+  const seconds = date.getSeconds()
+  return `${date.toDateString()} ${hour}:${minutes}:${seconds}} [${label}] ${level}: ${message}`
+})
+
+const logger = createLogger({
   level: 'info',
-  format: winston.format.json(),
+  format: combine(label({ label: 'um' }), timestamp(), myFormat, prettyPrint()),
   defaultMeta: { service: 'user-service' },
   transports: [
-    new winston.transports.Console(),
+    new transports.Console(),
 
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' }),
+    // new transports.File({
+    //   filename: path.join(process.cwd(), 'logs/winston/succeses', 'um-%DATE%-success.log'),
+    //   level: 'info',
+    // }),
+    new DailyRotateFile({
+      filename: path.join(
+        process.cwd(),
+        'logs/winston/succeses',
+        'um-%DATE%-success.log'
+      ),
+      datePattern: 'YYYY-DD-MM-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
+    }),
   ],
 })
-export default logger
+const errrorLogger = createLogger({
+  level: 'error',
+  format: combine(label({ label: 'um' }), timestamp(), myFormat, prettyPrint()),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    new transports.Console(),
+
+    // new transports.File({
+    //   filename: path.join(process.cwd(), 'logs/winston/errors', 'um-%DATE%-error.log'),
+    //   level: 'error',
+    // }),
+    new DailyRotateFile({
+      filename: path.join(
+        process.cwd(),
+        'logs/winston/errors',
+        'um-%DATE%-error.log'
+      ),
+      datePattern: 'YYYY-DD-MM-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
+    }),
+    // new winston.transports.File({ filename: 'combined.log' }),
+  ],
+})
+
+export { logger, errrorLogger }
